7f1a2304cf4023ecbf623b8b15e2b6ddd103bb20
rosseno <rosseno48@gmail.com>
2026-08-11 22:26:04 +0700

    Update app_smart_audit.py

--- app_smart_audit.py
+++ app_smart_audit.py
@@ -108,13 +108,13 @@ if menu_pilihan == "Dashboard Visualisasi & Tabel":
 
     col1, col2, col3, col4 = st.columns(4)
     with col1:
-        st.markdown(f'<div class="metric-card"><div class="metric-title">Total Temuan</div><div class="metric-value">{total_temuan}</div></div>', unsafe_allow_html=True)
+        st.markdown(f'<div class="metric-card"><div class="metric-title">TOTAL TEMUAN</div><div class="metric-value">{total_temuan}</div></div>', unsafe_allow_html=True)
     with col2:
-        st.markdown(f'<div class="metric-card"><div class="metric-title">Status BD (Belum Dilanjuti)</div><div class="metric-value">{jml_bd}</div></div>', unsafe_allow_html=True)
+        st.markdown(f'<div class="metric-card"><div class="metric-title">STATUS BD</div><div class="metric-value">{jml_bd}</div></div>', unsafe_allow_html=True)
     with col3:
-        st.markdown(f'<div class="metric-card"><div class="metric-title">Status EVAL (Evaluasi)</div><div class="metric-value">{jml_eval}</div></div>', unsafe_allow_html=True)
+        st.markdown(f'<div class="metric-card"><div class="metric-title">STATUS EVAL</div><div class="metric-value">{jml_eval}</div></div>', unsafe_allow_html=True)
     with col4:
-        st.markdown(f'<div class="metric-card"><div class="metric-title">Status SLS (Selesai)</div><div class="metric-value">{jml_sls}</div></div>', unsafe_allow_html=True)
+        st.markdown(f'<div class="metric-card"><div class="metric-title">STATUS SLS</div><div class="metric-value">{jml_sls}</div></div>', unsafe_allow_html=True)
 
     st.markdown("<br>", unsafe_allow_html=True)
 
